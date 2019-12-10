5b1cde0f4c93b1e8df915890fd1e429ffe783504
Przemysław Saracen <p.saracen@gmail.com>
2019-12-10 13:16:00 +0100

    Add test for storing single item

---
+++ tests/Feature/ItemTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+
+class ItemTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test storing single item.
+     */
+    public function testStoreItem()
+    {
+        $itemData = [
+            'field' => 'value'
+        ];
+        $response = $this->postJson('/api/items', $itemData);
+
+        $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertJson($itemData);
+    }
+}
